9e82a0d7451e278b34582407565e43723ba3d4c5
Anton Komarovskyi <anton.komarovskyi@sixt.com>
2024-05-22 14:38:23 +0200

    Refactored kafka listener and logs

---
+++ src/main/java/com/ankoma88/datastructures/Performance/kafka/KafkaMessageListener.java
@@ -0,0 +1,16 @@
+package com.ankoma88.datastructures.performance.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Component
+public class KafkaMessageListener {
+    private static final Logger LOGGER = Logger.getLogger(KafkaMessageListener.class.getName());
+
+    @KafkaListener(topics = "topic1", groupId = "group1", autoStartup = "false")
+    public void listen(String message) {
+        LOGGER.info(message);
+    }
+}
